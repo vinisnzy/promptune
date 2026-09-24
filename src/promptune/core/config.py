3d9197ad911939d907aa68713fe20e5aec76753e
Vinícius de Paula Moraes <viniciusmoraes295651@gmail.com>
2026-09-24 20:50:50 -0300

    feat: add jwt env vars

--- src/promptune/core/config.py
+++ src/promptune/core/config.py
@@ -8,6 +8,11 @@ BASE_DIR = Path(__file__).resolve().parents[3]
 
 class Settings(BaseSettings):
     database_url: str
+    jwt_secret: str
+    jwt_algorithm: str
+    access_token_expires_in_minutes: int = 15
+    refresh_token_expires_in_days: int = 7
+
     model_config = SettingsConfigDict(
         env_file=BASE_DIR / ".env", env_file_encoding="utf-8"
     )
